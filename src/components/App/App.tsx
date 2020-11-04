f2a7cee23ef8b3af90d6cce45df3b5224d5523b4
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-04 14:57:16 -0600

    Add idea box logic to demonstrate boilerplate abilities

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,62 @@
-import React from "react";
-import styles from "./App.module.scss"
+import React, { Component } from "react";
+import styles from "./App.module.scss";
+import IdeaForm from "../IdeaForm/IdeaForm";
 
-const App = (): JSX.Element => (
-  <h1 className={styles.header}>My React and TypeScript App!</h1>
-);
+export type Idea = {
+  id?: number;
+  title: string;
+  content: string;
+}
+
+type Ideas = Idea[];
+
+class App extends Component {
+  state: {
+    cards: Ideas;
+  }
+
+  constructor(props) {
+    super(props);
+    console.log(props, "where am I?")
+    this.state = {
+      cards: [
+        {
+          title: "Awesome Music Sharing App",
+          content: "What if we desinged an app to share music",
+        },
+        {
+          title: "Tasty new recipe",
+          content: "What if we mix pinaaple and jalapenos on pizza",
+        },
+      ],
+    };
+  }
+
+  renderCards = (): JSX.Element[] => {
+    return this.state.cards.map((card: Idea) => {
+      return (
+        <article className={styles.card} key={card.id}>
+          <h4>{card.title}</h4>
+          <p>{card.content}</p>
+        </article>
+      );
+    });
+  };
+
+  submitNewIdea = (newIdea: Idea): void => {
+    this.setState({cards: [...this.state.cards, newIdea]})
+  }
+
+  render(): JSX.Element {
+    return (
+      <section className={styles.main}>
+        <h1>IdeaBox</h1>
+        <IdeaForm submitIdea={this.submitNewIdea}/>
+        {this.renderCards()}
+      </section>
+    );
+  }
+}
 
 export default App;
+
